e1255bf8d2224a3621f92fd4c68d54b95a568520
dura4ok <84408595+shadowdaun@users.noreply.github.com>
2022-06-07 22:51:43 +0300

    Port scanner
    Updated, now multithreading

--- portchecker.py
+++ portchecker.py
@@ -1,5 +1,9 @@
 import socket
+import threading
+import concurrent.futures
 from colorama import Fore, init
+
+print_lock = threading.Lock()
 
 init()
 cgreen = Fore.GREEN
@@ -8,14 +12,24 @@ cred = Fore.RED
 cdef = Fore.WHITE
 chost = Fore.LIGHTBLUE_EX
 
-def checkport(host, port):
-    s = socket.socket()
+def singlechekport(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((host, port))
     except:
         return False
     else:
         return True
+
+def checkport(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        s.close()
+        with print_lock:
+            print(f"{cgreen}{host}:{port} is open {reset}")
+    except:
+        pass
 
 host = input("Enter a host:")
 while True:
@@ -25,21 +39,19 @@ while True:
         case '1':
             tport = input("Enter a port:")
             for port in range(int(tport), int(tport) + 1):
-                if(checkport(host, port)):
+                if(singlechekport(host, port)):
                     print(f"{cgreen}{host}:{port} is open {reset}")
                 else:
-                    print(f"{cred}{host}:{port} is closed{reset}")
+                    print(f"{cred}{host}:{port} is closed {reset}")
         case '2':
-            r1 = input("From:")
-            r2 = input("To:")
-            if(int(r1) < 0 or int(r2) > 65535):
+            r1 = input("From (1-65535):")
+            r2 = input("To (1-65535):")
+            if(int(r1) < 0 or int(r2) > 65535 or (r1 > r2)):
                 print("Incorrent value!")
-            else:  
-                for port in range(int(r1), int(r2) + 1):
-                    if(checkport(host, port)):
-                        print(f"{cgreen}{host}:{port} is open {reset}")
-                    else:
-                        print(f"{cred}{host}:{port} is closed{reset}")
+            else:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=777) as executor:
+                    for port in range(int(r1), int(r2)):
+                        executor.submit(checkport, host, port)  
         case '3':
             host = input("Enter a host:")
         case '4':
